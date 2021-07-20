ee61664da41bdf128fbb676bdf7ddc069c31df7e
Chi-Shen Wu <s97403002a@gmail.com>
2021-07-20 23:37:43 +0800

    實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表(View)，並且匯入 EDMX

--- MVCHomework/Models/CustomerProfile.Context.cs
+++ MVCHomework/Models/CustomerProfile.Context.cs
@@ -31,6 +31,7 @@ namespace MVCHomework.Models
         public virtual DbSet<客戶資料> 客戶資料 { get; set; }
         public virtual DbSet<客戶銀行資訊> 客戶銀行資訊 { get; set; }
         public virtual DbSet<客戶聯絡人> 客戶聯絡人 { get; set; }
+        public virtual DbSet<客戶View表> 客戶View表 { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
