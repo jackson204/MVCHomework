42e0b64d414c58fc4545ad09c6d04ab201d486b3
Chi-Shen Wu <s97403002a@gmail.com>
2021-07-22 23:22:29 +0800

    add Create 與 Edit 表單要套用欄位驗證 (必填、Email格式、欄位長度限制等驗證)

--- MVCHomework/Models/客戶資料.Partial.cs
+++ MVCHomework/Models/客戶資料.Partial.cs
@@ -32,7 +32,7 @@ namespace MVCHomework.Models
         [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
         public string 地址 { get; set; }
         
-        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [EmailAddress(ErrorMessage="這不是emil格式")]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
